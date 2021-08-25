8e2e0c636329e53170798e849fc0860e5b5430ee
graceDev1 <grace.devolop@gmail.com>
2021-08-25 16:00:58 +0200

    setup tailwind css

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div >
+      <Head>
+        <title>Blog-app</title>
+      </Head>
+      <h1 className="text-center font-serif mt-10">Hello world</h1>
+    </div>
+  )
+}
+
+export default Home
